ccf8825e8f6fa42a2ae2392e088b886bbcd18cb1
suganyasai1026 <suganyasai1026@gmail.com>
2025-04-07 14:34:48 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 const urbanist = Urbanist({
   subsets: ["latin"],
   weight: ["400", "600", "800"],
@@ -14,10 +9,6 @@ const urbanist = Urbanist({
 });
 
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
